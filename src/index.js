28a50f0c378a0402de59e911406254d9c8f7f5f1
William Wan <122927183+WillWan728@users.noreply.github.com>
2024-07-29 15:02:37 +0100

    fixed error with index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Navbar from "./Navbar";
-import App from "./App";
+import App from "./app";
 import Footer from "./footer";
 
 ReactDOM.render(<App />, document.getElementById('root'));
