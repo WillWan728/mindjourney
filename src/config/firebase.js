16716e7d028743dfffb9af43336193115e603032
William Wan <122927183+WillWan728@users.noreply.github.com>
2024-08-31 00:18:40 +0100

    .env file added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import {getAuth} from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1xCU3os0daWWtfTJN9TxH87KAWhnIEOo",
-  authDomain: "mindjourney-e72bf.firebaseapp.com",
-  projectId: "mindjourney-e72bf",
-  storageBucket: "mindjourney-e72bf.appspot.com",
-  messagingSenderId: "201009747630",
-  appId: "1:201009747630:web:a078e9b4ae5707afceff3f",
-  measurementId: "G-G9SRZV5EQJ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
